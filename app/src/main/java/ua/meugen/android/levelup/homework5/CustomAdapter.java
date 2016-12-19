ba06e34c42b984c828a902b20fb57fccb699cdfc
emalyshev <emalyshev@wellink.ru>
2016-12-19 15:57:17 +0200

    Implemented recycle view with checked text views

---
+++ app/src/main/java/ua/meugen/android/levelup/homework5/CustomAdapter.java
@@ -0,0 +1,56 @@
+package ua.meugen.android.levelup.homework5;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckedTextView;
+import android.widget.TextView;
+
+import java.util.List;
+
+
+public final class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.VH> {
+
+    private final LayoutInflater inflater;
+    private List<String> items;
+
+    public CustomAdapter(final Context context) {
+        this.inflater = LayoutInflater.from(context);
+    }
+
+    public void reload(final List<String> items) {
+        this.items = items;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public VH onCreateViewHolder(final ViewGroup parent, final int viewType) {
+        return new VH(inflater.inflate(R.layout.item, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(final VH holder, final int position) {
+        holder.setText(items.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public static final class VH extends RecyclerView.ViewHolder {
+
+        private final CheckedTextView checkedTextView;
+
+        public VH(final View itemView) {
+            super(itemView);
+            checkedTextView = (CheckedTextView) itemView.findViewById(R.id.text);
+        }
+
+        public void setText(final CharSequence text) {
+            checkedTextView.setText(text);
+        }
+    }
+}
--- app/src/main/java/ua/meugen/android/levelup/homework5/MainActivity.java
+++ app/src/main/java/ua/meugen/android/levelup/homework5/MainActivity.java
@@ -2,12 +2,61 @@ package ua.meugen.android.levelup.homework5;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
-public class MainActivity extends AppCompatActivity {
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public final class MainActivity extends AppCompatActivity {
+
+    private static final Random RANDOM = new Random();
+    private static final int COUNT = 100;
+
+    private static final String ITEMS_KEY = "items";
+
+    private CustomAdapter adapter;
+    private List<String> items;
+
+    private RecyclerView recyclerView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            this.items = genDumpData();
+        } else {
+            this.items = savedInstanceState.getStringArrayList(ITEMS_KEY);
+        }
+
+        this.recyclerView = (RecyclerView) findViewById(R.id.recycler);
+        this.recyclerView.setLayoutManager(new CustomLayoutManager(this));
+        setupAdapter();
+    }
+
+    private static List<String> genDumpData() {
+        final int count = Math.max(10, RANDOM.nextInt(COUNT));
+        final List<String> result = new ArrayList<>(count);
+        for (int i = 0; i < count; i++) {
+            result.add(new BigInteger(20, RANDOM).toString(26));
+        }
+        return result;
+    }
+
+    @Override
+    protected void onSaveInstanceState(final Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putStringArrayList(ITEMS_KEY, (ArrayList<String>) this.items);
+    }
+
+    private void setupAdapter() {
+        if (this.adapter == null) {
+            this.adapter = new CustomAdapter(this);
+            this.recyclerView.setAdapter(this.adapter);
+        }
+        this.adapter.reload(this.items);
     }
 }
